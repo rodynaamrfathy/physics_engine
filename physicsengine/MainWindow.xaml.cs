d149180fbbed14239849e36a3193047a00fa4df9
Rodyna Amr <130939400+rodynaamrfathy@users.noreply.github.com>
2024-08-14 15:27:00 +0300

    Update MainWindow.xaml.cs

--- physicsengine/MainWindow.xaml.cs
+++ physicsengine/MainWindow.xaml.cs
@@ -1,25 +1,24 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using SlimDX;
 
 namespace physicsengine
 {
+
     public partial class MainWindow : Window
     {
+
         private bool isDragged = false;
         private Ellipse ball;
-        private Vector3 position;
-        private Vector3 velocity = new Vector3(0, 0, 0);
-        private Vector3 gravity = new Vector3(0, 9.8f, 0);
-        private float bounceFactor = 0.7f;
-        private bool isMoving;
-        private Point clickPosition;
-        private double canvasHeight;
+        private Point currentPosition;
+        double s0, v0 = 0, t = 0, a = 9.8;
+        double bouncingfactor = 0.7, ballbottom = 0, canvasHeight;
+        bool IsMoving;
+        Vector3 x = new Vector3(0, 0.1f, 0);
 
         public MainWindow()
         {
@@ -38,76 +37,80 @@ namespace physicsengine
                 StrokeThickness = 1,
             };
             ballcanvas.Children.Add(ball);
-            canvasHeight = ballcanvas.ActualHeight;
-            position = new Vector3((float)(ballcanvas.ActualWidth / 2 - ball.Width / 2), (float)(canvasHeight - ball.Height), 0);
-            Canvas.SetLeft(ball, position.X);
-            Canvas.SetTop(ball, position.Y);
+            Canvas.SetTop(ball, ballcanvas.ActualHeight);
             ball.MouseLeftButtonDown += Ball_MouseLeftButtonDown;
             ball.MouseLeftButtonUp += Ball_MouseLeftButtonUp;
             ballcanvas.MouseMove += Ballcanvas_MouseMove;
-        }
 
+        }
         private void Ball_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             isDragged = true;
-            velocity = new Vector3(0, 0, 0); // Reset velocity
-            clickPosition = e.GetPosition(ball); // Store the position where the ball was clicked
+            v0 = 0;
+            t = 0;
+            a = 9.8;
+            bouncingfactor = 0.7;
             ball.CaptureMouse();
         }
-
         private void Ball_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             if (isDragged)
             {
                 isDragged = false;
                 ball.ReleaseMouseCapture();
-                isMoving = true;
-                Task.Run(StartFreeFall);
-            }
-        }
 
+                s0 = Canvas.GetTop(ball);
+
+                IsMoving = true;
+                Task.Run(() =>
+                {
+                    StartFreeFall();
+                });
+            }
+
+        }
         private void Ballcanvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (isDragged)
             {
-                var mousePos = e.GetPosition(ballcanvas);
-                position.X = (float)(mousePos.X - clickPosition.X);
-                position.Y = (float)(mousePos.Y - clickPosition.Y);
-                Canvas.SetLeft(ball, position.X);
-                Canvas.SetTop(ball, position.Y);
+                var mousepos = e.GetPosition(ballcanvas);
+                Canvas.SetLeft(ball, mousepos.X - currentPosition.X);
+                Canvas.SetTop(ball, mousepos.Y - currentPosition.Y);
             }
         }
-
         private void StartFreeFall()
         {
-            const float dt = 0.016f; // Time step (~16ms for 60 FPS)
-            while (isMoving)
+            while (IsMoving)
             {
                 Dispatcher.Invoke(() =>
                 {
-                    // Update velocity and position
-                    velocity += gravity * dt;
-                    position += velocity * dt;
+                    double s = s0 + v0 * t + 0.5 * a * t * t;
+                    double v = v0 + a * t;
+                    Canvas.SetTop(ball, s);
 
-                    // Check for collision with the bottom of the canvas
-                    if (position.Y + ball.Height >= canvasHeight)
+
+                    ballbottom = s + ball.Height;
+                    canvasHeight = ballcanvas.ActualHeight;
+                    // one the ball hits the end of the canvas it will star bouncing
+                    if (ballbottom >= canvasHeight)
                     {
-                        // Bounce
-                        velocity.Y = -velocity.Y * bounceFactor;
-                        position.Y = (float)(canvasHeight - ball.Height); // Reset position just above the bottom
+                        v0 = -v * bouncingfactor;
+                        s0 = canvasHeight - ball.Height;
 
-                        // Stop if the bounce is negligible
-                        if (Math.Abs(velocity.Y) < 1)
-                            isMoving = false;
+                        t = 0.16;
+
+                        // if the abs of v0 is less than 1 stop the ball
+                        if (Math.Abs(v) < 1)
+                            IsMoving = false;
+
                     }
+                    else
+                        t += 0.16;
 
-                    // Apply the new position to the ball
-                    Canvas.SetLeft(ball, position.X);
-                    Canvas.SetTop(ball, position.Y);
                 });
-
-                Thread.Sleep((int)(dt * 1000)); // Maintain frame rate
+                Thread.Sleep(10);
             }
         }
+
     }
 }
