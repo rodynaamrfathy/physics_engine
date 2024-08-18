f1bc9d0bc93ff76fc941d9da9c08a97855bd997b
Rodyna Amr <130939400+rodynaamrfathy@users.noreply.github.com>
2024-08-18 13:05:41 +0300

    Add files via upload

---
+++ physicsengine/Ball.cs
@@ -0,0 +1,28 @@
+﻿using SlimDX;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Shapes;
+namespace physicsengine
+{
+    internal class Ball : Shapes
+    {
+        public float Radius { get; set; }
+
+        public Ball(float mass, System.Drawing.Color color, float radius,float canvasHeight) : base(mass, color)
+        {
+            // Set the initial position of the ball
+            Position = new Vector3(0, canvasHeight + radius * 2, 0);
+            Radius = radius;
+            DrawingShape = new Ellipse()
+            {
+                Width = Radius * 2,
+                Height = Radius * 2,
+                Fill = new SolidColorBrush(System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B)),
+                Stroke = Brushes.Black,
+                StrokeThickness = 1
+            };
+        }
+    }
+}
---
+++ physicsengine/Shapes.cs
@@ -0,0 +1,67 @@
+﻿using SlimDX;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace physicsengine
+{
+    internal class Shapes
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Velocity { get; set; }
+        public float BouncingFactor { get; set; }
+        public Shape DrawingShape { get; set; }
+        public float Mass { get; set; }
+        public Color Color { get; set; }
+
+        public Shapes(float mass, Color color)
+        {
+            Velocity = new Vector3(0, 0, 0);
+            BouncingFactor = 0.7f;
+            Mass = mass;
+            Color = color;
+        }
+
+        public Vector3 GravityForce()
+        {
+            return new Vector3(0, Mass * 9.8f, 0);
+        }
+
+        public Vector3 FrictionForce()
+        {
+            float frictionCoefficient = 0.1f;
+            return new Vector3(-frictionCoefficient * Velocity.X, 0, -frictionCoefficient * Velocity.Z);
+        }
+
+        public List<Vector3> TotalForces()
+        {
+            List<Vector3> forces = new List<Vector3>
+            {
+                GravityForce(),
+                FrictionForce()
+            };
+            return forces;
+        }
+
+        public void UpdatePosition(float deltaTime)
+        {
+            Vector3 gravityForce = GravityForce();
+
+            // Update velocity based on gravity
+            Velocity += gravityForce * deltaTime / Mass;
+
+            // Update position based on velocity
+            Position += Velocity * deltaTime;
+
+            // Update the drawing shape position
+            if (DrawingShape != null)
+            {
+                Canvas.SetTop(DrawingShape, Position.Y);
+                Canvas.SetLeft(DrawingShape, Position.X);
+            }
+        }
+    }
+
+}
