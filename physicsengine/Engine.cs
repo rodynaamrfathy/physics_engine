8c72f989cd5ff102e9189984037b8e1c294ccce7
Rodyna Amr <130939400+rodynaamrfathy@users.noreply.github.com>
2024-08-18 16:07:53 +0300

    Add files via upload

---
+++ physicsengine/Engine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace physicsengine
+{
+    internal class Engine
+    {
+        private List<Shapes> shapes;
+        public Engine() 
+        {
+            shapes = new List<Shapes>();
+        }
+
+
+        public void AddShape(Shapes shape)
+        {
+            shapes.Add(shape);
+        }
+
+        public void Update(float deltaTime, float canvasHeight, float canvasWidth)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.UpdatePosition(deltaTime, canvasHeight, canvasWidth);
+            }
+        }
+        
+    }
+}
