43a5eca865a376f5ef22ab74d34acb75567fdeda
Rodyna Amr <130939400+rodynaamrfathy@users.noreply.github.com>
2024-08-18 11:13:39 +0000

    Update Ball.cs

--- physicsengine/Ball.cs
+++ physicsengine/Ball.cs
@@ -1,7 +1,8 @@
-﻿using SlimDX;
+using SlimDX;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 namespace physicsengine
@@ -14,6 +15,7 @@ namespace physicsengine
         {
             // Set the initial position of the ball
             Position = new Vector3(0, canvasHeight + radius * 2, 0);
+            Velocity = new Vector3(0, 0, 0);
             Radius = radius;
             DrawingShape = new Ellipse()
             {
@@ -23,6 +25,43 @@ namespace physicsengine
                 Stroke = Brushes.Black,
                 StrokeThickness = 1
             };
+        }
+
+
+        public void UpdatePosition(float deltaTime, float canvasHeight, float canvasWidth)
+        {
+            Vector3 gravityForce = GravityForce();
+
+            // Update velocity based on gravity
+            Velocity += gravityForce * deltaTime / Mass;
+
+            // Update position based on velocity
+            Position += Velocity * deltaTime;
+
+            // Check if the ball hits the top of the canvas
+            if (Position.Y <= 0)
+            {
+                Position = new Vector3(Position.X, 0, Position.Z);
+                Velocity = new Vector3(Velocity.X, -Velocity.Y * BouncingFactor, Velocity.Z);
+            }
+            // Check if the ball hits the right edge of the canvas
+            if (Position.X + Radius * 2 >= canvasWidth)
+            {
+                Position = new Vector3(canvasWidth - Radius * 2, Position.Y, Position.Z);
+                Velocity = new Vector3(-Velocity.X * BouncingFactor, Velocity.Y, Velocity.Z);
+            }
+            // Check if the ball hits the left edge of the canvas
+            if (Position.X <= 0)
+            {
+                Position = new Vector3(0, Position.Y, Position.Z);
+                Velocity = new Vector3(-Velocity.X * BouncingFactor, Velocity.Y, Velocity.Z);
+            }
+            // Update the drawing shape position
+            if (DrawingShape != null)
+            {
+                Canvas.SetTop(DrawingShape, Position.Y);
+                Canvas.SetLeft(DrawingShape, Position.X);
+            }
         }
     }
 }
