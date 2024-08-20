66fdbedef3a31d9ad3cecf3dbd417c696e84b4fb
Rodyna Amr <130939400+rodynaamrfathy@users.noreply.github.com>
2024-08-20 07:43:36 +0000

    Update Engine.cs

--- physicsengine/Engine.cs
+++ physicsengine/Engine.cs
@@ -1,32 +1,40 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace physicsengine
 {
     internal class Engine
     {
         private List<Shapes> shapes;
-        public Engine() 
+
+        public Engine()
         {
             shapes = new List<Shapes>();
         }
-
 
         public void AddShape(Shapes shape)
         {
             shapes.Add(shape);
         }
 
-        public void Update(float deltaTime, float canvasHeight, float canvasWidth)
+        public Shapes GetShapeFromDrawing(Shape drawingShape)
+        {
+            return shapes.FirstOrDefault(s => s.DrawingShape == drawingShape);
+        }
+
+        public void Update(float deltaTime, float canvasHeight, float canvasWidth, bool IsMoving)
         {
             foreach (var shape in shapes)
             {
-                shape.UpdatePosition(deltaTime, canvasHeight, canvasWidth);
+                shape.UpdatePosition(deltaTime, canvasHeight, canvasWidth, IsMoving);
             }
         }
-        
     }
+
+
 }
