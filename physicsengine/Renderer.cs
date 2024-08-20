1079f3ce3347fb7f6e099928a740e4b78470c2ce
Rodyna Amr <130939400+rodynaamrfathy@users.noreply.github.com>
2024-08-20 07:43:18 +0000

    Update Renderer.cs

--- physicsengine/Renderer.cs
+++ physicsengine/Renderer.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace physicsengine
 {
@@ -13,17 +15,42 @@ namespace physicsengine
 
         public Renderer(Canvas canvas)
         {
-            this.canvas = canvas; 
+            this.canvas = canvas;
         }
 
         public void AddShapeToCanvas(Shapes shape)
         {
             canvas.Children.Add(shape.DrawingShape);
         }
-        //public void UpdateCanvas()
-        //{
 
-        //}
+        public void UpdateShapePosition(Shapes shape)
+        {
+            Canvas.SetLeft(shape.DrawingShape, shape.Position.X);
+            Canvas.SetTop(shape.DrawingShape, shape.Position.Y);
+        }
 
+        public void SetInitialShapePosition(Shapes shape)
+        {
+            Canvas.SetLeft(shape.DrawingShape, (float) 0.5 * ( canvas.ActualWidth + shape.DrawingShape.Width));
+            Canvas.SetTop(shape.DrawingShape, (float) canvas.ActualHeight - shape.DrawingShape.Height);
+        }
+
+        public void UpdateCanvas()
+        {
+            foreach (UIElement child in canvas.Children)
+            {
+                if (child is Shape drawingShape)
+                {
+                    var shape = canvas.Children.OfType<Shapes>().FirstOrDefault(s => s.DrawingShape == drawingShape);
+                    if (shape != null)
+                    {
+                        UpdateShapePosition(shape);
+                    }
+                }
+            }
+        }
     }
+
+
+
 }
